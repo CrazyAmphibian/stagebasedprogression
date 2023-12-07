52d49a52c3203eb5d2d1a33791c9d930554216a7
CrazyAmphibian <0crazyamphibian@gmail.com>
2023-12-07 13:20:51 -0800

    bug fix, more hard
    increased exponential dificulty from 2.5% -> 5%
    fixed level cap not being correcty applied
    made multyplayer scaling affect initial difficulty

--- StageProgression/StageProgression.cs
+++ StageProgression/StageProgression.cs
@@ -29,23 +29,25 @@ namespace StageProgression
 				float diffscaling = difficultyDef.scalingValue;
 				int stageclears = self.stageClearCount;
 				int playercount = self.participatingPlayerCount;
+				float playerfactor = (float)playercount * .3f + .7f;
+
 				float coeff = 0f;
-				coeff += 2.25f*diffscaling*.5f/3f; //add an additional 1.25 blocks. this makes maps a bit harder when you first spawn to account for the difficulty never scaling up.
 
-				float playerfactor = (float)playercount * .3f + .7f; //increased player scaling from .3x to .5x per player.
-				//nevermind. reverted that. makes multiplayer a bit hard.
+				coeff += playerfactor*2.25f *diffscaling*.5f/3f; //add an additional 1.25 blocks. this makes maps a bit harder when you first spawn to account for the difficulty never scaling up.
+
+				
 				//float stagefactor = (float)stageclears*1.5f;
-				float stagefactor = (1f+0.025f*((float)stageclears-1f))*(float)stageclears*1.25f; //for each stage after the first, add 2.5% to the difficulty.
-
+				float stagefactor = (1f+0.05f*((float)stageclears-1f))*(float)stageclears*1.25f; //for each stage after the first, add 5% to the difficulty.
+				
 				coeff += stagefactor * diffscaling * playerfactor/3f; //no time here, boss!
 				//we have to divide by 3 here because 1 coeff is the big bars, so this makes it the smaller bars
 				//these settings get it so that you get 2.5 "blocks" every stage (plus a bonus 2.5% after more stages)
-				coeff = Mathf.Min(coeff, (float)Run.ambientLevelCap);
+				//coeff = Mathf.Min(coeff, (float)Run.ambientLevelCap);
 				self.difficultyCoefficient = coeff;
 				self.compensatedDifficultyCoefficient = coeff;
 
 				//self.ambientLevel = 1 + (coeff - playerfactor) / .33f;
-				self.ambientLevel =  (coeff ) / .33f; //removed subtracting playerfactor.
+				self.ambientLevel = Mathf.Min((float)Run.ambientLevelCap, (coeff ) / .33f); //removed subtracting playerfactor.
 				int ambientLevelFloor = Mathf.FloorToInt(self.ambientLevel);
 
 				if (ambientLevelFloor != 0 && self.ambientLevelFloor > ambientLevelFloor) //check for ambient leveling up.
